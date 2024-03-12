53a95f89dafd8fac0900ccc5cf2b7a54687c970c
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-03-12 11:07:27 +0100

    created demo of project (scraping data, then creating model to predict prices)

---
+++ model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+data = pd.read_csv('collect_data.csv')
+
+X = data.drop(columns=['Cena za m2'])
+y = data['Cena za m2']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
+categorical_features = X.select_dtypes(include=['object']).columns
+
+numeric_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ])
+
+model = Pipeline(steps=[('preprocessor', preprocessor),
+                        ('regressor', RandomForestRegressor())])
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+print('Mean Absolute Error:', mae)
---
+++ scrap.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import csv
+
+url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/?page="
+
+
+def get_links(numbers_of_pages):
+    pages = range(1, numbers_of_pages)
+    links = []
+    for page in pages:
+        response = requests.get(url + str(page))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        offers = soup.find_all('a', class_='css-rc5s2u')
+        for l in offers:
+            # skipping otodom offers, because for some reason i can't scrap otodom webpages
+            if l['href'].startswith('http'):
+                # links.append(l['href'])
+                pass
+            else:
+                links.append('https://www.olx.pl' + l['href'])
+
+    return links
+
+
+def get_params(offer_url):
+    # stopping thread for 2 seconds to prevent DDoS olx :)
+    time.sleep(2)
+    response = requests.get(offer_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    params = soup.find_all('p', class_='css-b5m1rv er34gjf0')
+
+    return params
+
+
+# napisać te funkcje ładniej-narazie jest napisana tylko tak, aby działało
+def write_to_csv(params, writer):
+    new_params = []
+    for param in params[1:-1]:
+        new_params.append(param.text
+                          .replace('zł/', '')
+                          .replace('m²', '')
+                          .replace(' ', '')
+                          .replace('pokoje', '')
+                          .replace('iwięcej', '')
+                          .replace('pokój', '')
+                          .replace('pokój', '')
+                          .replace(',', '.')
+                          .strip()
+                          .split(':')
+                          )
+
+    if len(new_params) == 7:
+        print(new_params)
+        writer.writerow(
+            [
+                float(new_params[0][1]),
+                new_params[1][1],
+                new_params[2][1],
+                new_params[3][1],
+                new_params[4][1],
+                float(new_params[5][1]),
+                int(new_params[6][1])
+            ])
+    else:
+        print('skipped: ', new_params)
+
+
+def run():
+    number_of_pages = 26
+    links_to_offers = get_links(number_of_pages)
+    file = open('collect_data.csv', 'w')
+    writer = csv.writer(file)
+    writer.writerow(['Cena za m2', 'Poziom', 'Umeblowanie', 'Rynek', 'Rodzaj zabudowy', 'Powierzchnia', 'Liczba pokoi'])
+
+    for link in links_to_offers:
+        params = get_params(link)
+        write_to_csv(params, writer)
+
+
+if __name__ == "__main__":
+    # scraping all oferts from
+    # run()
+    print()
